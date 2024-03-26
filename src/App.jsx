971d2b0613569954661f51926ad58434a02c990f
pjroggy <pierrejeanroggy@gmail.com>
2024-03-26 12:02:17 +0100

    add title

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import HelloReactWorld from './component/HelloReactWorld'
+import MyTitle from './component/MyTitle'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,6 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <HelloReactWorld />
+      <MyTitle />
     </>
   )
   
