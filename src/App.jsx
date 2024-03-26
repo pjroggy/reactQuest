e100849c57b8ee1864291f28626dbb589b1c4ec5
pjroggy <pierrejeanroggy@gmail.com>
2024-03-26 16:57:11 +0100

    challenge react basic04

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from 'react'
+
 import './App.css'
 import PokemonCard from './component/PokemonCard'
 
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+const pokemon = { name: "mew" };
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-          <PokemonCard />
+        <PokemonCard pokemon={pokemon}/>
       </div>
     </>
   )
-  
 }
 
 export default App
--- src/component/PokemonCard.jsx
+++ src/component/PokemonCard.jsx
@@ -1,6 +1,4 @@
-function PokemonCard() {
-
-    const pokemon = pokemonList[0];
+function PokemonCard({pokemon}) {
 
     return <figure>
         {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
@@ -8,15 +6,6 @@ function PokemonCard() {
     </figure>
 }
 
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
+
 
 export default PokemonCard;
